9bd800f2a460979fd9fd2c3d3a228caf891ec11c
Bipin Kumar Chaurasia <bipin@mplgaming.com>
2022-04-10 22:10:51 +0530

    refactored functional args

--- src/statement.js
+++ src/statement.js
@@ -1,7 +1,7 @@
 function statement(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
-        result += ` ${playFor(plays, perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
     }
     result += `Amount owed is ${usd(totalAmount()/100)}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -46,7 +46,7 @@ function statement(invoice, plays) {
 
     function amountFor(aPerformance) {
         let result = 0;
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -61,7 +61,7 @@ function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
         return result;
     }
