44215123a7b5886bb238814496b071a4e40957eb
Bipin Kumar Chaurasia <bipin@mplgaming.com>
2022-04-10 20:32:06 +0530

    refactored credits for value in functions

--- src/statement.js
+++ src/statement.js
@@ -1,21 +1,34 @@
-function amountFor(perf, play) {
+function amountFor(aPerformance, play) {
     let result = 0;
     switch (play.type) {
         case "tragedy":
             result = 40000;
-            if (perf.audience > 30) {
-                result += 1000 * (perf.audience - 30);
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
             result = 30000;
-            if (perf.audience > 20) {
-                result += 10000 + 500 * (perf.audience - 20);
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
             throw new Error(`unknown type: ${play.type}`);
+    }
+    return result;
+}
+
+function playFor(plays, aPerformance) {
+    return plays[aPerformance.playID];
+}
+
+function volumeCreditsFor(plays, aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(plays, aPerformance).type) {
+        result += Math.floor(aPerformance.audience / 5);
     }
     return result;
 }
@@ -30,11 +43,9 @@ function statement(invoice, plays) {
         minimumFractionDigits: 2
     }).format;
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = amountFor(perf, play);
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        let thisAmount = amountFor(perf, playFor(plays, perf));
+        volumeCredits += volumeCreditsFor(plays, perf)
+        result += ` ${playFor(plays, perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`;
